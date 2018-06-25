f16dd8419e086e2112d86854e94c118f379638ec
manan mehta <mehtamanan0@gmail.com>
2018-06-25 15:37:06 +0100

    add new polybar modules

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -6,5 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar example &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
